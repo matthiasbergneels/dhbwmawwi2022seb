95e307e677e836f96e71045e6970a3ee9314ea02
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2023-06-01 19:29:29 +0200

    Comparable & Comparator Example

--- src/lecture/chapter9/SetExample.java
+++ src/lecture/chapter9/SetExample.java
@@ -2,10 +2,7 @@ package lecture.chapter9;
 
 import com.sun.source.tree.Tree;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetExample {
 
@@ -57,13 +54,51 @@ public class SetExample {
 
         personSet.add(new Person("Klaus", "Zeus", 35));
         personSet.add(new Person("Gabi", "Mueller", 33));
+      personSet.add(new Person("Gabi", "Mueller", 32));
         personSet.add(new Person("Nina", "Rexi", 39));
         personSet.add(myPerson);
-        personSet.add(new Person("Michael", "Mayer", 22));
-        personSet.add(myPerson);
+      personSet.add(new Person("Michael", "Mayer", 22));
+      personSet.add(new Person("Victor", "Mayer", 22));
+      personSet.add(myPerson);
 
         for(Person currentPerson : personSet){
             System.out.println(currentPerson);
         }
+
+      List<Person> myPersonList = new ArrayList<Person>(personSet);
+      myPersonList.sort(new SortPersonByNameCharacterCount());
+
+      System.out.println("Sortiert nach Anzahl Zeichen:");
+      for(Person currentPerson : myPersonList){
+        System.out.println(currentPerson);
+      }
+
+      myPersonList.sort((o1, o2)->{
+        return o1.getAge() - o2.getAge();
+      });
+
+      System.out.println("Sortiert nach Alter:");
+      for(Person currentPerson : myPersonList){
+        System.out.println(currentPerson);
+      }
+
+      Collections.sort(myPersonList); // --> natürliche Ordnung
+      Collections.sort(myPersonList, new SortPersonByNameCharacterCount()); // --> Sortierung nach beliebiger externe Ordnung
+
+
+
+
+      System.out.println("Sortiert nach Name:");
+      myPersonList.sort(new Person.SortByName());
+      for(Person currentPerson : myPersonList){
+        System.out.println(currentPerson);
+      }
+
+
+      System.out.println("Sortiert nach Name:");
+      myPersonList.sort(Person.sortByName);
+      for(Person currentPerson : myPersonList){
+        System.out.println(currentPerson);
+      }
     }
 }
