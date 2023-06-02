c4eb64d7118cb6e60cbc536d38f05432f36e50d6
NickB42069 <nick.bermbach@gmail.com>
2023-06-02 13:46:02 +0200

    genau

--- src/studentarea/excersises/chapter9/Kunde.java
+++ src/studentarea/excersises/chapter9/Kunde.java
@@ -1,6 +1,59 @@
 package studentarea.excersises.chapter9;
+import java.util.Comparator;
+public class Kunde implements Comparable<Kunde>{
+    private String name;
+    private String vorname;
+    private int kundennummer;
+    public static Comparator<Kunde> compareName = (k1, k2) ->{
+        if (!k1.name.equalsIgnoreCase(k2.name)){
+            return k1.name.toUpperCase().compareTo(k2.name.toUpperCase());
+        }
+        if (!k1.vorname.equalsIgnoreCase(k2.vorname)){
+            return k1.vorname.toUpperCase().compareTo(k2.vorname.toUpperCase());
+        }
+        return 0;
+    };
+    public Kunde(String name, String vorname, int kundennummer) {
+        this.name = name;
+        this.vorname = vorname;
+        this.kundennummer = kundennummer;
+    }
 
-public class Kunde {
+    public String getName() {
+        return name;
+    }
 
-    // TODO: Implementieren :-)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public int getKundennummer() {
+        return kundennummer;
+    }
+
+    public void setKundennummer(int kundennummer) {
+        this.kundennummer = kundennummer;
+    }
+
+    @Override
+    public int compareTo(Kunde o) {
+        return this.kundennummer - o.getKundennummer();
+    }
+    @Override
+    public boolean equals(Object o){
+        if (o.getClass() != this.getClass()) return false;
+        if (!this.name.equalsIgnoreCase(((Kunde) o).name)) return false;
+        if (!this.vorname.equalsIgnoreCase(((Kunde) o).vorname)) return false;
+        if (this.kundennummer != ((Kunde) o).kundennummer) return false;
+        return true;
+        //so würde ich das in die Production schicken
+    }
 }
---
+++ src/studentarea/excersises/chapter9/TestKunde.java
@@ -0,0 +1,47 @@
+package studentarea.excersises.chapter9;
+
+import java.util.*;
+
+public class TestKunde {
+    public static void main(String[] args) {
+        Kunde K1 = new Kunde("Mustermann", "Klaus", 4711);
+        Kunde K2 = new Kunde("Beispiel", "Hans", 5180);
+        Kunde K3 = new Kunde("Mustermann", "Hilde", 4712);
+        Kunde K4 = new Kunde("Vorbild", "Theodor", 8278);
+        Kunde K5 = new Kunde("Dummy", "Jimmy", 1111);
+
+        Kunde K11 = new Kunde("Mustermann", "Klaus", 4711);
+
+        Set<Kunde> KundenSet = new TreeSet<>();
+        KundenSet.add(K1);
+        KundenSet.add(K2);
+        KundenSet.add(K3);
+        KundenSet.add(K4);
+        KundenSet.add(K5);
+
+        //TreeSet Ausgabe
+        System.out.println("TreeSet");
+        Iterator<Kunde> iS = KundenSet.iterator();
+        while(iS.hasNext()){
+            Kunde aKunde = (Kunde) iS.next();
+            System.out.println("Kunde: " + aKunde.getVorname() +" "+ aKunde.getName() +" ("+ aKunde.getKundennummer() + ")");
+        }
+
+
+        List<Kunde> KundenVektor = new ArrayList<>(KundenSet);
+        KundenVektor.sort(Kunde.compareName);
+
+        //Vektor Ausgabe
+        System.out.println("Vektor");
+        Iterator<Kunde> iV = KundenVektor.iterator();
+        while(iV.hasNext()){
+            Kunde aKunde = (Kunde) iV.next();
+            System.out.println("Kunde: " + aKunde.getVorname() +" "+ aKunde.getName() +" ("+ aKunde.getKundennummer() + ")");
+        }
+
+        //equals-Methode
+        System.out.println(K1.equals(new Object()));
+        System.out.println(K1.equals(K2));
+        System.out.println(K1.equals(K11));
+    }
+}
