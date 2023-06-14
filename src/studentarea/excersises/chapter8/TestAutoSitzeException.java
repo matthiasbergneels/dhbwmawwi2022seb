4b09c67374a3288ecef8487cb08bc9bb9e1489d0
Sebastian Kiebert <sebastian.kiebert@sap.com>
2023-06-14 14:07:41 +0200

    Created Class to test AutoSitze-Class. Triggering an error

---
+++ src/studentarea/excersises/chapter8/TestAutoSitzeException.java
@@ -0,0 +1,17 @@
+package studentarea.excersises.chapter8;
+
+public class TestAutoSitzeException {
+  public static void main(String[] args) {
+    try {
+
+      new AutoSitze(true, "Blau");
+      System.out.println("Ging alles gut");
+
+    } catch (FalscherParameterException ex) {
+      System.out.println(ex.getMessage());
+      System.out.println("Die Kombination ist fehlgeschlagen.");
+    }
+
+
+  }
+}
