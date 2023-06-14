51cf9159145dd2a5e9034426dc4a92e741e4d81e
Sebastian Kiebert <sebastian.kiebert@sap.com>
2023-06-14 14:05:54 +0200

    Created AutoSitze Class. Add methods and condition

---
+++ src/studentarea/excersises/chapter8/AutoSitze.java
@@ -0,0 +1,25 @@
+package studentarea.excersises.chapter8;
+
+public class AutoSitze {
+  private final boolean isbezug;
+  private final String color;
+
+  public AutoSitze(boolean isbezug, String color) throws FalscherParameterException {
+
+    if (isbezug && !(color.equals("Schwarz") || color.equals("Weiß"))) {
+      throw new FalscherParameterException(isbezug, color);
+    }
+
+    this.isbezug = isbezug;
+    this.color = color;
+
+  }
+
+  public boolean isLederBezug() {
+    return isbezug;
+  }
+
+  public String getColor() {
+    return color;
+  }
+}
