90dd5cb5089db03357ecc1f7b9e5e572b2dc5381
Sebastian Kiebert <sebastian.kiebert@sap.com>
2023-06-14 14:19:44 +0200

    Demo data created

---
+++ src/studentarea/excersises/chapter9/TestKunde.java
@@ -0,0 +1,12 @@
+package studentarea.excersises.chapter9;
+
+
+public class TestKunde {
+  Kunde kunde1 = new Kunde("Klaus", "Mustermann", 4711);
+  Kunde kunde2 = new Kunde("Hans", "Beispiel", 5180);
+  Kunde kunde3 = new Kunde("Hilde", "Mustermann", 4712);
+  Kunde kunde4 = new Kunde("Dummy", "Vorbild", 8278);
+  Kunde kunde5 = new Kunde("Jimmy", "Theodor", 1111);
+
+
+}
