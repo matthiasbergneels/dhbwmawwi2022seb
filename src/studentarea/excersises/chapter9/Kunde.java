20a7cd8b873861ce3a9b07165784c26f41e324c9
Sebastian Kiebert <sebastian.kiebert@sap.com>
2023-06-14 14:17:58 +0200

    Implemented Kunde-Class. Creating Setter and Getter

--- src/studentarea/excersises/chapter9/Kunde.java
+++ src/studentarea/excersises/chapter9/Kunde.java
@@ -3,4 +3,43 @@ package studentarea.excersises.chapter9;
 public class Kunde {
 
     // TODO: Implementieren :-)
+
+  private String name;
+  private String vorname;
+  private int kundenNummer;
+
+  public Kunde(String name, String vorname, int nummer){
+    this.name = name;
+    this.vorname = vorname;
+    this.kundenNummer = nummer;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getName() {
+    return name;
+  }
+
+  public void setVorname(String vorname) {
+    this.vorname = vorname;
+  }
+  public String getVorname() {
+    return vorname;
+  }
+
+  public void setKundenNummer(int kundenNummer) {
+    this.kundenNummer = kundenNummer;
+  }
+  public int getKundenNummer() {
+    return kundenNummer;
+  }
+
+
+
+
+
+
+
+
 }
