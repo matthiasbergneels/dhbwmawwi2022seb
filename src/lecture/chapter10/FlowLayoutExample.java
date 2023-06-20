0c2d0b014e76aa2e059d534d9eb23792eb0ba6cf
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2023-06-20 13:02:54 +0200

    LayoutManager Examples

---
+++ src/lecture/chapter10/FlowLayoutExample.java
@@ -0,0 +1,29 @@
+package lecture.chapter10;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutExample extends JFrame {
+
+  private FlowLayoutExample(){
+    super();
+    this.setTitle("FlowLayout Example");
+
+    this.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+    this.add(new JLabel("Text1"));
+    this.add(new JLabel("Text2"));
+    this.add(new JLabel("Text3"));
+    this.add(new JLabel("Text4"));
+    this.add(new JLabel("Text5"));
+    this.add(new JLabel("Text6"));
+
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.pack();
+    this.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    new FlowLayoutExample();
+  }
+}
