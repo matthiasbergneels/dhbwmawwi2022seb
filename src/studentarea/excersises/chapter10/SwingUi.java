740ea56edbed5520648e33ad8a3cb2593f4d0825
Jens Feser <jens.feser@gmx.de>
2023-06-20 15:35:27 +0200

    SwingUi-exercise Vorlesung

---
+++ src/studentarea/excersises/chapter10/SwingUi.java
@@ -0,0 +1,64 @@
+package studentarea.excersises.chapter10;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
+public class SwingUi extends JFrame {
+
+  private SwingUi(){
+
+    this.setTitle("Logon");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(new BorderLayout());
+
+    JPanel centerPanel = new JPanel(new FlowLayout());
+    centerPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+
+
+    JPanel grid1 = new JPanel(new GridLayout(0,2));
+    Border line = new LineBorder(Color.BLACK,2);
+
+    grid1.setBorder(new TitledBorder(line, "Verbindungen"));
+
+    grid1.add(new JLabel("User:"));
+    grid1.add(new JTextField());
+    grid1.add(new JLabel("Passwort:"));
+    grid1.add(new JPasswordField());
+    grid1.add(new JLabel("Art:"));
+    grid1.add(new JComboBox(new String[]{"FTP"}));
+    grid1.add(new JLabel("Host:"));
+    grid1.add(new JTextField());
+    grid1.add(new JLabel("Port:"));
+    grid1.add(new JTextField());
+
+    JPanel grid2 = new JPanel(new GridLayout());
+    grid2.setBorder(new TitledBorder(line, "Dateien"));
+
+    grid2.add(new JLabel("Quelle:"));
+    grid2.add(new JTextField());
+    grid2.add(new JLabel("Ziel:"));
+    grid2.add(new JTextField());
+
+    centerPanel.add(grid1);
+    centerPanel.add(grid2);
+
+    JPanel bottomPanel = new JPanel(new FlowLayout());
+
+    bottomPanel.add(new JButton("OK"));
+    bottomPanel.add(new JButton("Cancel"));
+
+    this.add(centerPanel, BorderLayout.CENTER);
+    this.add(bottomPanel, BorderLayout.SOUTH);
+
+    this.pack();
+    this.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    SwingUi ui = new SwingUi();
+  }
+}
