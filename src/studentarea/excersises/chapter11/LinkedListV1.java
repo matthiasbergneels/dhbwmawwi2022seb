b48b818b1715cf801fb29404e87096d3ec08deef
alex-meinecke <alexandermeinecke@icloud.com>
2023-07-06 21:16:08 +0200

    Linked List Implementierung von Vorlesung 06.08.22

---
+++ src/studentarea/excersises/chapter11/LinkedListV1.java
@@ -0,0 +1,181 @@
+package studentarea.excersises.chapter11;
+
+public class LinkedListV1<T> {
+
+
+  private Node firstNode;
+
+  void add(T data){
+    Node newNode = new Node(data);
+
+    if(firstNode == null){
+      firstNode = newNode;
+      return;
+    }
+
+    Node currentNode = firstNode;
+    while(currentNode.nextNode != null){
+      currentNode = currentNode.getNextNode();
+    }
+
+    currentNode.setNextNode(newNode);
+  }
+
+  private Node goToIndex(int index) throws IndexOutOfBoundsException{
+    Node currentNode = firstNode;
+
+    if (isEmty()){
+      throw new IndexOutOfBoundsException();
+    }
+
+    if (index > size() - 1) {
+      throw new IndexOutOfBoundsException();
+    }
+
+    for (int i = 0; i < index; i++) {
+      currentNode = currentNode.nextNode;
+    }
+
+    return currentNode;
+
+  }
+
+  public T get(int index) throws IndexOutOfBoundsException {
+    if (isEmty()){
+      throw new IndexOutOfBoundsException();
+    }
+
+    if (index > size() - 1) {
+      throw new IndexOutOfBoundsException();
+    }
+
+    Node currentNode = goToIndex(index);
+
+    return currentNode.data;
+  }
+
+  public void setData(int index, T data) throws IndexOutOfBoundsException {
+
+    Node newNode = new Node(data);
+
+    if (index == 0) {
+
+      newNode.nextNode = firstNode.nextNode;
+      firstNode = newNode;
+      return;
+    }
+
+    Node oldNode = goToIndex(index);
+    Node oldNodePrev = goToIndex(index - 1);
+
+    newNode.nextNode = oldNode.nextNode;
+    oldNodePrev.nextNode = newNode;
+  }
+
+  public boolean contains(T data){
+    Node currentNode = firstNode;
+
+    if (isEmty()) {
+      return false;
+    }
+
+    while (currentNode.nextNode != null) {
+
+      if (currentNode.data.equals(data)) {
+        return true;
+      }
+
+      currentNode = currentNode.nextNode;
+    }
+
+    return false;
+  }
+
+  public boolean remove(T data){
+    Node currentNode = firstNode;
+    int indexOfDel = 0;
+
+    if (isEmty()) {
+      return false;
+    }
+
+    if (contains(data) == false) {
+      return false;
+    }
+
+
+    while (currentNode.nextNode != null) {
+
+
+      if (currentNode.data.equals(data)) {
+        if (currentNode == firstNode) {
+          firstNode = firstNode.nextNode;
+          return true;
+        }
+
+
+        Node prev = goToIndex(indexOfDel - 1);
+        prev.nextNode = currentNode.nextNode;
+        return true;
+      }
+      currentNode = currentNode.nextNode;
+      indexOfDel++;
+
+    }
+
+
+    return false;
+  }
+
+  public boolean isEmty() {
+    if (firstNode == null){
+      return true;
+    } else return false;
+
+  }
+
+  public int size(){
+    int size = 0;
+
+    if (isEmty()) {
+      return size;
+    }
+
+    Node currentNode = firstNode;
+
+    size = 1;
+
+    while (currentNode.nextNode != null) {
+      currentNode = currentNode.nextNode;
+      size++;
+    }
+
+    return size;
+
+  }
+
+
+
+  private class Node{
+    T data;
+    Node nextNode;
+
+    Node(T data){
+      this.data = data;
+    }
+
+    T getData(){
+      return data;
+    }
+
+    void setNextNode(Node nextNode){
+      this.nextNode = nextNode;
+    }
+
+    Node getNextNode(){
+      return nextNode;
+    }
+
+  }
+
+}
---
+++ src/studentarea/excersises/chapter11/TestLinkedListV1.java
@@ -0,0 +1,50 @@
+package studentarea.excersises.chapter11;
+
+public class TestLinkedListV1 {
+
+  public static void main(String[] args) {
+    LinkedListV1<String> list = new LinkedListV1<>();
+    System.out.println("Is Empty:" + list.isEmty());
+
+    list.add("1");
+    list.add("2");
+    list.add("3");
+    list.add("4");
+    list.add("5");
+    list.add("6");
+
+    System.out.println("Is Empty:" + list.isEmty());
+    System.out.println("Contains 2:" + list.contains("2"));
+    System.out.println("Size:" + list.size());
+
+    printListContent(list);
+
+    System.out.println("Contains 1:" + list.contains("1"));
+
+    list.remove("1");
+    list.setData(0, "44");
+    list.setData(2, "4567");
+
+
+
+    printListContent(list);
+
+
+
+
+  }
+
+  public static void printListContent(LinkedListV1 list) {
+    try {
+      for (int i = 0; i < list.size(); i++) {
+        System.out.println(list.get(i));
+      }
+
+    } catch (IndexOutOfBoundsException e) {
+      System.out.println(e.getMessage());
+
+    }
+
+  }
+
+}
