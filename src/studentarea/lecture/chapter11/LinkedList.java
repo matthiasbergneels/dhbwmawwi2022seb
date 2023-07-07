9f45b34b90a449f01ce56b07e4f1c0c648190fd9
Joel Spanagel <Joel.Spanagel@web.de>
2023-07-07 12:09:02 +0200

    Commit

--- src/studentarea/lecture/chapter11/LinkedList.java
+++ src/studentarea/lecture/chapter11/LinkedList.java
@@ -4,54 +4,63 @@ import org.junit.platform.engine.support.hierarchical.Node;
 
 public class LinkedList<T> {
 
-
-
     private Node firstNode;
 
-    void add(T data){
-        Node newNode = new Node (data);
+    void add(T data) {
+        Node newNode = new Node(data);
 
-        if(firstNode != null){
+        if (firstNode != null) {
             firstNode = newNode;
             return;
 
         }
 
         Node currentNode = firstNode;
-        while(currentNode.nextNode != null){
-            currentNode=  currentNode.getNextNode();
+        while (currentNode.nextNode != null) {
+            currentNode = currentNode.getNextNode();
         }
 
         currentNode.setNextNode(newNode);
     }
-    
-    public T get(int Index){
+
+    public T get(int Index) {
         return null;
     }
 
-    
+    public void setData(int index, T data) {
 
+    }
 
+    public boolean contains(T data) {
+        return false;
+    }
 
-    private class Node{
+    public boolean remove(T data) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    private class Node {
         T data;
         Node nextNode;
 
-        Node(T data){
+        Node(T data) {
             this.data = data;
 
         }
 
-        T getData(){
+        T getData() {
             return data;
         }
 
-        void setNextNode(Node nextNode){
+        void setNextNode(Node nextNode) {
             this.nextNode = nextNode;
         }
 
-
-        Node getNextNode(){
+        Node getNextNode() {
             return nextNode;
         }
     }
