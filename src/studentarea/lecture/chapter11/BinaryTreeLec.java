456ef24038bfcf42f7231985688a7dcdcac89fcd
Joel Spanagel <Joel.Spanagel@web.de>
2023-07-07 16:28:37 +0200

    commit

---
+++ src/studentarea/lecture/chapter11/BinaryTreeLec.java
@@ -0,0 +1,50 @@
+package studentarea.lecture.chapter11;
+
+
+public class BinaryTreeLec<T extends Comparable> {
+
+  private Node root = null;
+  private int size = 0;
+
+  public boolean add(T data){
+    return false;
+  }
+
+  public int size(){
+    return this.size;
+  }
+
+  private class Node{
+    private final T data;
+    private Node leftNode;
+    private Node rightNode;
+
+    public Node(T data){
+      this.data = data;
+      this.leftNode = null;
+      this.rightNode = null;
+    }
+
+    public T getData(){
+      return this.data;
+    }
+
+    public Node getLeftNode(){
+      return this.leftNode;
+    }
+
+    public Node getRightNode(){
+      return this.rightNode;
+    }
+
+    public void setLeftNode(Node leftNode){
+      this.leftNode = leftNode;
+    }
+
+    public void setRightNode(Node rightNode){
+      this.rightNode = rightNode;
+    }
+  }
+
+}
+
