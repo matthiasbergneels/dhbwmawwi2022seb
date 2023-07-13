05df7a46f86d0f5252639f8bf53906155b9169fc
samiri123 <amirisoliaman@gmail.com>
2023-07-13 15:56:00 +0200

    changes

--- src/studentarea/lecture/chapter11/BinaryTree.java
+++ src/studentarea/lecture/chapter11/BinaryTree.java
@@ -39,7 +39,69 @@ public class BinaryTree<T extends Comparable> {
   }
 
 
+  public void printInOrder(){
+    if(root == null){
+      System.out.println("Der Baum ist leer.");
+      return;
+    }
+    printInOrder(root);
+  }
+  private void printInOrder(Node currentNode){
+    if(currentNode.getLeftNode() != null){
+      printInOrder(currentNode.getLeftNode());
+    }
+    System.out.println(currentNode.getData());
+    if(currentNode.getRightNode() != null){
+      printInOrder(currentNode.getRightNode());
+    }
+  }
 
+  // pre-order -> current - left - right
+  public void printPreOrder(){
+    if(root == null){
+      System.out.println("Der Baum ist leer.");
+      return;
+    }
+    // TODO
+    printPreOrder(root);
+  }
+
+  private void printPreOrder(Node currentNode){
+    if (currentNode != null) {
+      printPreOrder(currentNode);
+    }
+    System.out.println(currentNode.getData());
+    if (currentNode.getLeftNode() != null) {
+      printPreOrder(currentNode.getLeftNode());
+    }
+    if(currentNode.getRightNode() != null){
+      printPreOrder(currentNode.getRightNode());
+    }
+  }
+
+  // post-order -> left - right - current
+  public void printPostOrder(){
+    if(root == null){
+      System.out.println("Der Baum ist leer.");
+      return;
+    }
+    // TODO
+    printPostOrder(root);
+  }
+
+  private void printPostOrder(Node currentNode){
+
+    if (currentNode.getLeftNode() != null) {
+      printPreOrder(currentNode.getLeftNode());
+    }
+    if(currentNode.getRightNode() != null){
+      printPreOrder(currentNode.getRightNode());
+    }
+    if (currentNode != null) {
+      printPreOrder(currentNode);
+    }
+    System.out.println(currentNode.getData());
+  }
 
   public int size() {
     return this.size;
