5436b1d34f77e678615b068d2b2e6c2441ef3c70
alex-meinecke <alexandermeinecke@icloud.com>
2023-07-13 15:40:37 +0200

    Optimized and normal Bubble Sort => Optimized time comparison

--- src/studentarea/excersises/chapter12/TestAlgorithms.java
+++ src/studentarea/excersises/chapter12/TestAlgorithms.java
@@ -5,29 +5,35 @@ public class TestAlgorithms {
     Integer[] unsortetData = {44,1,56,102,33,2,3,99,678,7};
     Long start;
     Long stop;
+    Long averageDifferencePc10k = Long.valueOf(0);
+
+    for (int i = 0; i < 10000; i++) {
+      System.out.println("Normal Bubble Sort");
+      start = System.nanoTime();
+      Algorithmen.optBubbleSort(unsortetData);
+      stop = System.nanoTime();
+
+      Long normalBubbleSortTime = stop - start;
+      System.out.println("Normal Bubble Sort: " + normalBubbleSortTime + " ns");
+
+      System.out.println("Optimized Bubble Sort");
+      start = System.nanoTime();
+      Algorithmen.optBubbleSort(unsortetData);
+      stop = System.nanoTime();
+
+      Long optimizedBubbleSortTime = stop - start;
+
+      System.out.println("Optimized Bubble Sort: " + optimizedBubbleSortTime + " ns");
+
+      averageDifferencePc10k = averageDifferencePc10k + normalBubbleSortTime / optimizedBubbleSortTime * 100;
 
 
-    System.out.println("Normal Bubble Sort");
-    start = System.nanoTime();
-    Algorithmen.optBubbleSort(unsortetData);
-    stop = System.nanoTime();
 
-    Long normalBubbleSortTime = stop - start;
-    System.out.println("Normal Bubble Sort: " + normalBubbleSortTime + " ns");
+    }
 
-    System.out.println("Optimized Bubble Sort");
-    start = System.nanoTime();
-    Algorithmen.optBubbleSort(unsortetData);
-    stop = System.nanoTime();
+    averageDifferencePc10k = averageDifferencePc10k / 10000;
 
-    Long optimizedBubbleSortTime = stop - start;
-
-    System.out.println("Optimized Bubble Sort: " + optimizedBubbleSortTime + " ns");
-
-    Long diffrencePC = normalBubbleSortTime / optimizedBubbleSortTime * 100;
-
-    System.out.println("Difference between normal and optimized Bubble Sort : " + diffrencePC + " %");
-
+    System.out.println("Difference between normal and optimized Bubble Sort : " + averageDifferencePc10k + " %");
 
     /*
     for (int i: sortetData
