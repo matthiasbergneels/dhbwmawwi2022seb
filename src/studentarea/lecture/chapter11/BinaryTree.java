d2e81ed425bd26136f4c5f26407f72fac1dd9322
Hajny25 <daniel.hajnal5@gmail.com>
2023-07-13 15:15:15 +0200

    add binary tree

--- src/studentarea/lecture/chapter11/BinaryTree.java
+++ src/studentarea/lecture/chapter11/BinaryTree.java
@@ -1,8 +1,30 @@
 package studentarea.lecture.chapter11;
 
-public class BinaryTree<T> {
+public class BinaryTree<T extends Comparable<T>> {
 
   private Node root;
+
+  public void add(T data) {
+    addRecursive(root, data);
+  }
+
+  private void addRecursive(Node node, T data) {
+    if (data.compareTo(node.data) < 0) {
+      if (node.left == null) {
+        node.left = new Node(data);
+        return;
+      }
+      addRecursive(node.left, data);
+    }
+    else {
+      if (node.right == null) {
+        node.right = new Node(data);
+        return;
+      }
+      addRecursive(node.right, data);
+    }
+  }
+
   private class Node {
     T data;
     Node left;
